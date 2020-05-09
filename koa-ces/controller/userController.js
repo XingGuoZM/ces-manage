551d8efbbbb3eda9697729e715c45219d4ec6deb
李志铭 <wb-lzm559047@alibaba-inc.com>
2020-05-09 18:44:01 +0800

    userController

--- koa-ces/controller/userController.js
+++ koa-ces/controller/userController.js
@@ -1,27 +1,27 @@
 const UserModel =require('../model/user')
+// 使用mock数据，操作mock/user.json文件
 const user=require('../mock/user.json')
+
 class UserController{
     static async query(ctx){
-        // let info =await UserModel.query(ctx.request.body)
-        console.log(1111)
         ctx.body={code:200,msg:'查询成功',data:user}
     }
     static async add(ctx){
         let obj=ctx.request.body
         obj.interst=obj.interst.join()
         obj.id=new Date().getTime()
-        let info = await UserModel.add(ctx.request.body)
-        ctx.body=info
+        // let info = await UserModel.add(ctx.request.body)
+        ctx.body={code:200,msg:'新增成功'}
     }
     static async edit(ctx){
         let obj=ctx.request.body
         obj.interst=obj.interst.join()
-        let info = await UserModel.edit(obj)
-        ctx.body=info
+        // let info = await UserModel.edit(obj)
+        ctx.body={code:200,msg:'编辑成功'}
     }
     static async del(ctx){
-        let info = await UserModel.del(ctx.request.body)
-        ctx.body=info
+        // let info = await UserModel.del(ctx.request.body)
+        ctx.body={code:200,msg:'删除成功'}
     }
 }
 module.exports= UserController
