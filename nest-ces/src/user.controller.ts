5964fc2c01d9b586b875df7c84f46b6aac4df551
XingGuoZM <18969176324@163.com>
2020-10-25 20:50:52 +0800

    controller

--- nest-ces/src/user.controller.ts
+++ nest-ces/src/user.controller.ts
@@ -5,19 +5,19 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly UserService: UserService) {}
 
-  @Get('user/query')
+  @Get('api/user/query')
   queryUser(): string {
     return this.UserService.queryUser();
   }
-  @Get('user/add')
+  @Get('api/user/add')
   addUser(): string {
     return this.UserService.addUser();
   }
-  @Get('user/edit')
+  @Get('api/user/edit')
   editUser(): string {
     return this.UserService.editUser();
   }
-  @Get('user/del')
+  @Get('api/user/del')
   delUser(): string {
     return this.UserService.delUser();
   }
